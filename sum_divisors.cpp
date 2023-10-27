9ee7191fb156001caba9c8d41a3be237d135ddb7
Ayush Singh <ayush@intervue.io>
2023-10-28 02:25:30 +0530

    AS - added problem statemnet

--- sum_divisors.cpp
+++ sum_divisors.cpp
@@ -3,6 +3,21 @@
 #include <vector>
 using namespace std;
 
+/*
+ *
+ * Find all integers between m and n (m and n integers with 1 <= m <= n) such
+ * that the sum of their squared divisors is itself a square. We will return an
+ * array of subarrays or of tuples (in C an array of Pair) or a string. The
+ * subarrays (or tuples or Pairs) will have two elements: first the number the
+ * squared divisors of which is a square and then the sum of the squared
+ * divisors.
+ *
+ *
+ * if 22 divides 44
+ * then sum of 22
+ *
+ */
+
 static long long allDivisors(long long num) {
   long long sum = 0;
   for (long long i = 1; i <= num; i++) {
@@ -31,10 +46,9 @@ static vector<pair<long long, long long>> listSquared(long long m,
 int main() {
 
   vector<pair<long long, long long>> sq_list = listSquared(1, 250);
-
-  // printf("size: %ld\n", sq_list.size());
-  // for (int i = 0; i < sq_list.size(); i++) {
-  // printf("%lld\t%lld\n", sq_list[i].first, sq_list[i].second);
-  //}
+  printf("size: %ld\n", sq_list.size());
+  for (int i = 0; i < sq_list.size(); i++) {
+    printf("%lld\t%lld\n", sq_list[i].first, sq_list[i].second);
+  }
   return 0;
 }
